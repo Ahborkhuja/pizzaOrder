6845a41baa5e8be6b7e855c81bd5d448601f5007
ahborkhuja <93854287+Ahborkhuja@users.noreply.github.com>
2022-11-27 15:54:25 +0500

    final commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector("#form");
 const content = document.querySelector("#content");
 const Uname = document.querySelector("#name");
@@ -29,6 +30,7 @@ const formHandler = (e) => {
   else {
     orders.push(order);
   }
+  // for push value
   add.forEach((item) => {
     if (item.checked) order.add.push(item.value);
   });
@@ -38,6 +40,21 @@ const formHandler = (e) => {
   ingredients.forEach((item) => {
     if (item.checked) order.ingredient.push(item.value);
   });
+  // Clearing process
+  Uname.value = null;
+  Pnum.value = null;
+  address.value = null;
+  Pnum.value = null;
+  Pwidth.value = "Thin";
+  add.forEach((item) => {
+    if (item.checked) item.checked=false;
+  });
+  size[0].checked=true;
+  size[1].checked=false;
+  size[2].checked=false;
+  ingredients.forEach((item) => {
+    if (item.checked) item.checked=false;
+  });
   display();
 }
 function display() {
@@ -45,37 +62,38 @@ function display() {
 
   for (let i = 0; i < orders.length; i++) {
     result += `
-    <div class="card position-relative" style="width: 18rem;">
-    <button type="button" onclick="deleteOrder(${orders[i].id})" class="delete position-absolute top-0 end-0"></button>
-    <div class="card-body">
-      <h5 class="card-title">Order: ${i + 1}</h5>
-    </div>
-    <div>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">
-        <ul>
-          <li class="m-1"><strong>Name: </strong>${orders[i].name}</li>
-          <li class="m-1"><strong>Phone: </strong>+998${orders[i].PhoneNum}</li>
-          <li class="m-1"><strong>Address: </strong>${orders[i].address}</li>
-        </ul>
-      </li>
-      <li class="list-group-item">
+    <div class="card position-relative fit m-3" style="width: 18rem;">
+        <button type="button" onclick="deleteOrder(${orders[i].id})"
+          class="delete position-absolute top-0 end-0"></button>
+        <div class="card-body">
+          <h5 class="card-title">Order: ${i + 1}</h5>
+        </div>
+        <div>
+        </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item p-0">
-            <ul class="mb-4">
-              <li class="m-1"><strong>Dough thickness: </strong>${orders[i].thickness}</li>
-              <li class="m-1"><strong>Size: </strong>${orders[i].size} sm</li>
-              <li class="m-1"><strong>On pizza: </strong>${orders[i].ingredient.join(", ")}</li>
-              <li class="m-1"><strong>Add: </strong>${orders[i].add.join(", ")}</li>
+          <li class="list-group-item">
+            <ul>
+              <li class="m-1"><strong>Name: </strong>${orders[i].name}</li>
+              <li class="m-1"><strong>Phone: </strong>+998${orders[i].PhoneNum}</li>
+              <li class="m-1"><strong>Address: </strong>${orders[i].address}</li>
             </ul>
           </li>
-          <li class="list-group-item text-end pe-5"><strong>Total: </strong>$${total(orders[i].id)}</li>
+          <li class="list-group-item">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item p-0">
+                <ul class="mb-4">
+                  <li class="m-1"><strong>Dough thickness: </strong>${orders[i].thickness}</li>
+                  <li class="m-1"><strong>Size: </strong>${orders[i].size} sm</li>
+                  <li class="m-1"><strong>On pizza: </strong>${orders[i].ingredient.join(", ")}</li>
+                  <li class="m-1"><strong>Add: </strong>${orders[i].add.join(", ")}</li>
+                </ul>
+              </li>
+              <li class="list-group-item text-end pe-5"><strong>Total: </strong>$${total(orders[i].id)}</li>
+            </ul>
+          </li>
         </ul>
-  </div>
-</div>
+      </div>
       `;
-
   }
   content.innerHTML = result;
 }
@@ -87,23 +105,23 @@ function deleteOrder(elementId) {
   display();
 }
 function total(elementId) {
-let sum=0 
+  let sum = 0
   let findOrder = orders.find((element) => +element.id == elementId);
   switch (+findOrder.size) {
-    case 30:sum+=12;break;
-    case 35:sum+=15;break;
-    default:sum+=10;break;
+    case 30: sum += 12; break;
+    case 35: sum += 15; break;
+    default: sum += 10; break;
   }
   console.log(sum);
   switch (findOrder.thickness) {
-    case "Thick":sum+=15;break;
-    case "Medium":sum+=12;break;
-    default:sum+=10;break;
+    case "Thick": sum += 15; break;
+    case "Medium": sum += 12; break;
+    default: sum += 10; break;
   }
   console.log(sum);
-  sum+=findOrder.ingredient.length*5;
+  sum += findOrder.ingredient.length * 5;
   console.log(sum);
-  sum+=findOrder.add.length*3;
+  sum += findOrder.add.length * 3;
   return sum;
 }
 form.addEventListener("submit", formHandler);
